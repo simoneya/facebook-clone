4f5480b217d4aa6f6fbb5f1cc3f755cfe64c298d
simoneya <yavar.simona@yahoo.com>
2023-01-06 15:12:33 +0000

    update

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import "./Header.css";
 import SearchIcon from '@mui/icons-material/Search';
+import HomeIcon from '@mui/icons-material/Home';
+import FlagIcon from '@mui/icons-material/Flag';
+import SubscriptionsOutlinedIcon from '@mui/icons-material/SubscriptionsOutlined';
+import StorefrontOutlinedIcon from '@mui/icons-material/StorefrontOutlined';
+import SupervisedUserCircleOutlinedIcon from '@mui/icons-material/SupervisedUserCircleOutlined';
+import AddIcon from '@mui/icons-material/Add';
+import ForumIcon from '@mui/icons-material/Forum';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Avatar, IconButton } from '@mui/material';
 
 
 function Header() {
@@ -13,8 +23,41 @@ function Header() {
                 <input type="text"/>
             </div>
         </div>
-        <div className="header__main"></div>
-        <div className="header__right"></div>
+        <div className="header__main">
+            <div className="header__option">
+                 <HomeIcon fontSize="large" />
+            </div>
+            <div className="header__option">
+                 <FlagIcon fontSize="large" />
+            </div>
+            <div className="header__option">
+                 <SubscriptionsOutlinedIcon fontSize="large" />
+            </div>
+            <div className="header__option">
+                 <StorefrontOutlinedIcon fontSize="large" />
+            </div>
+            <div className="header__option">
+                 <SupervisedUserCircleOutlinedIcon fontSize="large" />
+            </div>
+        </div>
+        <div className="header__right">
+            <div className="header__info">
+                <Avatar />
+                <h4>Simona Ya</h4>
+            </div>
+            <IconButton>
+                <AddIcon />
+            </IconButton>
+            <IconButton>
+                <ForumIcon />
+            </IconButton>
+            <IconButton>
+                <NotificationsActiveIcon />
+            </IconButton>
+            <IconButton>
+                <ExpandMoreIcon />
+            </IconButton>
+        </div>
     </div>
   )
 }
