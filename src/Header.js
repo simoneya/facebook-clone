ad1087823195991a630335ab4aeaecc6a1bfb323
simoneya <yavar.simona@yahoo.com>
2023-01-07 11:16:25 +0000

    update

--- src/Header.js
+++ src/Header.js
@@ -20,7 +20,7 @@ function Header() {
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1024px-Facebook_Logo_%282019%29.png" alt= ""/>
             <div className="header__input">
                 <SearchIcon />
-                <input type="text"/>
+                <input placeholder="Search Facebook" type="text"/>
             </div>
         </div>
         <div className="header__center">
