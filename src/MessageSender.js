27fc9b8705032b470d975bae895354d0e3ba0ab8
simoneya <yavar.simona@yahoo.com>
2023-01-08 11:11:21 +0000

    update

--- src/MessageSender.js
+++ src/MessageSender.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import "./MessageSender.css";
+import { Avatar } from '@mui/material';
+import VideocamIcon from '@mui/icons-material/Videocam';
+import PhotoLibraryIcon from '@mui/icons-material/PhotoLibrary';
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
 
 function MessageSender() {
+
+    const handleSubmit = e => {
+        e.preventDefault();
+    }
+
   return (
     <div className="messageSender">
         <div className="messageSender__top">
-
+            <Avatar />
+            <form>
+                <input
+                  className="messageSender__input"
+                  placeholder={`What's on your mind?`}
+                />
+                <input placeholder="image URL (Optional)" />
+                <button onClick={handleSubmit} type="submit">
+                    Hidden Submit
+                </button>
+            </form>
         </div>
         <div className="messageSender__bottom">
-
+            <div className="messageSender__option">
+                <VideocamIcon style={{ color: "red" }} />
+                <h3>Live Video</h3>
+            </div>
+            <div className="messageSender__option">
+                <PhotoLibraryIcon style={{ color: "green" }} />
+                <h3>Photo/Video</h3>
+            </div>
+            <div className="messageSender__option">
+                <InsertEmoticonIcon style={{ color: "orange" }} />
+                <h3>Feeling/Activity</h3>
+            </div>
         </div>
     </div>
   )
