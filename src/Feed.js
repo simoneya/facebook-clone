5a22393f846fe2523e5757f59e61d2b47ab64e50
simoneya <yavar.simona@yahoo.com>
2023-01-09 11:34:39 +0000

    update

--- src/Feed.js
+++ src/Feed.js
@@ -10,11 +10,11 @@ function Feed() {
         <StoryReel />
         <MessageSender />
         <Post
-            profilePic={profilePic}
-            message={message}
-            timestamp={timestamp}
-            username={username}
-            image={image}
+            profilePic={"https://simonadev.co.uk/wp-content/uploads/2022/03/IMG_20211225_150234-1-768x1024.jpg"}
+            message="Wow this works"
+            timestamp="This is a timestamp"
+            username="simdev"
+            image={"https://www.creatopy.com/blog/wp-content/uploads/2019/07/where-are-stories-on-facebook-600x600.png"}
          />
         <Post />
         <Post />
