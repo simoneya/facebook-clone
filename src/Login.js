0e88bb8788c068e282dd2a63a6e0920347d6c5bd
simoneya <yavar.simona@yahoo.com>
2023-01-12 09:48:46 +0000

    adding stateProvider, data layer

--- src/Login.js
+++ src/Login.js
@@ -2,15 +2,22 @@ import React from 'react';
 import "./Login.css";
 import { Button } from '@mui/material';
 import { auth, provider } from "./firebase";
+import { actionTypes } from './reducer';
+import { useStateValue } from './StateProvider';
 
 function Login() {
+
+    cons [state, dispatch] = useStateValue();
 
     const signIn = () => {
        
       auth
       .signInWithPopup(provider)
       .then(result => {
-        console.log(result);
+        dispatch({
+          type: actionTypes.SET_USER,
+          user: result.user,
+        })
       }).catch(error => alert(error.message));
     };
 
